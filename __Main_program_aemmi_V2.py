64e49f83152b75454620a46a54914cf70cb295e8
Sarah Zarei <zarei.sarah@gmail.com>
2020-01-30 14:25:52 +0330

    up 4

--- __Main_program_aemmi_V2.py
+++ __Main_program_aemmi_V2.py
@@ -1,7 +1,7 @@
 __author__ = "Sara Zarei, Sajjad Aemmi"
 __copyright__ = "Copyright 2020"
 __license__ = "GPL"
-__version__ = "1.0.4"
+__version__ = "1.0.5"
 __email__ = "sajjadaemmi@gmail.com"
 __status__ = "Production"
 
