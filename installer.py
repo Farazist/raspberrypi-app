73b45cdd528a7719a137182f188cee71da1b0c86
Sajjad Aemmi <sajjadaemmi@gmail.com>
2020-05-04 23:53:32 +0430

    installer

--- installer.py
+++ installer.py
@@ -1,11 +1,9 @@
 import PyInstaller.__main__
 
-package_name = 'Farazist'
-
 PyInstaller.__main__.run([
     'main.py',
     '--hidden-import=PySide2.QtXml',
-    '--name=%s' % package_name,
+    '--name=Farazist',
     '--onefile',
     '--windowed',
 ])
