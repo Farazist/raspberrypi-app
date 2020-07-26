ef3e398831fcc22db6200c88c3f467595ad6a4c5
Sajjad Aemmi <sajjadaemmi@gmail.com>
2020-07-26 13:32:28 -0500

    rfid

---
+++ test/RFID_read.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+try:
+    id, text = reader.read()
+    print(id)
+    print(text)
+finally:
+    GPIO.cleanup()
