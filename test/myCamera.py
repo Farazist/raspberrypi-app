357452347f95c52a34daa112992bab8313c1b43b
Sajjad Aemmi <sajjadaemmi@gmail.com>
2020-07-31 05:44:03 +0430

    :|

--- test/myCamera.py
+++ test/myCamera.py
@@ -2,13 +2,13 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-#camera.rotation = 180
+camera.rotation = 180
 camera.resolution = (2592, 1944)
 camera.framerate = 15
 #camera.exposure_mode = 'beach'
-#camera.awb_mode = 'sunlight'
+# camera.awb_mode = 'auto'
 camera.start_preview()
 #camera.start_preview(fullscreen=False, window = (100, 20, 640, 480))
-sleep(300)
+sleep(5)
 camera.capture('/home/pi/Desktop/image.jpg')
 camera.stop_preview()
